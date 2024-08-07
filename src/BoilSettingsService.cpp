4668e27eb4671b5696ab0ee2a2aaeb0be70c74da
mvrinaldi <mvrinaldi@live.com>
2024-08-07 09:41:45 -0300

    Update BoilSettingsService.cpp

--- src/BoilSettingsService.cpp
+++ src/BoilSettingsService.cpp
@@ -1,17 +1,20 @@
 #include <BoilSettingsService.h>
 
 BoilSettingsService::BoilSettingsService(AsyncWebServer *server, FS *fs, BrewSettingsService *brewSettings)
-    : _brewSettings(brewSettings), BrewListService(server, fs,
-                                                   GET_BOIL_SETTINGS_SERVICE_PATH,
-                                                   POST_BOIL_SETTINGS_SERVICE_PATH,
-                                                   BOIL_SETTINGS_FILE) {}
+    : BrewListService(server, fs,
+                      GET_BOIL_SETTINGS_SERVICE_PATH,
+                      POST_BOIL_SETTINGS_SERVICE_PATH,
+                      BOIL_SETTINGS_FILE),
+      _brewSettings(brewSettings) {}
+
+
 
 bool BoilSettingsService::jsonSchemaIsValid(JsonDocument &jsonObj, String &messages)
 {
     JsonArray steps = jsonObj["st"];
     bool validJson = true;
 
-    for (int i = 0; i < steps.size(); i++)
+    for (std::size_t i = 0; i < steps.size(); i++)
     {
         JsonObject step = steps.getElement(i);
         if (step["n"] == "")
